bcd9d1f2179ecc508ff1442aa86bb87fb9e454de
FourSwordKirby <rogliu26@gmail.com>
2017-11-25 00:46:19 -0500

    added some base classes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int timeLimit;
+    public int currentTime;
+
+	// Use this for initialization
+	void Start () {
+        //Planning stuff?		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            currentTime++;
+            print("Next Timestep");
+        }
+	}
+}
